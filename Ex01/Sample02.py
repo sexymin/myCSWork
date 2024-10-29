7f52d96cce498fc38d3c5a32b7d4c62e43725a03
Adam <adam.oh@libertas-scholars.org>
2024-10-29 10:13:56 +0900

    updated

---
+++ Ex01/Sample02.py
@@ -0,0 +1,29 @@
+'''
+2
+4
+6
+8
+10
+12
+14
+16
+18
+3
+6
+9
+12
+15
+18
+21
+24
+27
+'''
+##############MAKE###############
+def gugudan(dan):
+    # Put your work here
+    for each in range(1,10,1):
+        print(each*dan)
+##############RUN################
+gugudan(2)
+gugudan(3)
+
