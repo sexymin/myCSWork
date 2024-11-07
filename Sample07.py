201f2c8e689fd9bf6abe5e16b3138380808fe782
sexymin <hwanmin.suh@libertas-scholars.org>
2024-11-07 09:54:33 +0900

    Add files via upload

--- Sample07.py
+++ Sample07.py
@@ -1,11 +1,12 @@
 items = ["Apple", "Banana", "Kiwi", "Orange"]
 price = [22.3, 54.1, 13.6, 4.6]
 
-
-def makeDict(given01 : list, given02: list) -> dict:
-    pass
-
-
+def makeDict(given01: list, given02: list) -> dict:
+    result = {}
+    for i in range(len(given01)):
+        result[given01[i]] = given02[i]
+    
+    return result
 
 ################## RUN ###################
 print(makeDict(items, price))
