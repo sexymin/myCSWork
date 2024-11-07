f16ef6f1b799023ae88e7b5aaf13519879b05db8
sexymin <hwanmin.suh@libertas-scholars.org>
2024-11-07 09:53:18 +0900

    Add files via upload

---
+++ Sample07.py
@@ -0,0 +1,11 @@
+items = ["Apple", "Banana", "Kiwi", "Orange"]
+price = [22.3, 54.1, 13.6, 4.6]
+
+
+def makeDict(given01 : list, given02: list) -> dict:
+    pass
+
+
+
+################## RUN ###################
+print(makeDict(items, price))
