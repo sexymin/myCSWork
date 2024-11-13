26d34463514d736235ee5b800fec11597629e1eb
sexymin <hwanmin.suh@libertas-scholars.org>
2024-11-13 15:31:07 +0900

    Add files via upload

--- Sample03.py
+++ Sample03.py
@@ -1,28 +1,51 @@
-'''
-Check if it is a triangle or not
-Print out the results and return nothing
-ex)
-True
-True
-False
-False
-True
-True
+bDays = ["05_07_06", "05_12_13", "07_08_17", "06_08_25", "06_09_27", "06_05_23", "07_10_01", "06_02_01", "06_07_05", "07_06_02", "06_03_17", "05_11_17", "06_04_03", "07_07_11"]
+names = ["Daeju", "Richard", "Kevin", "James", "Warren", "Hwanmin", "Donghyuk", "Taehoon", "Taeyoon", "Beomkyu", "Sieon", "Devyn", "Seoyoung", "Hayoung"]
+name_birthday_dict = dict(zip(names, bDays))
 
 '''
-triangleList = [(3,3,3), (3,4,5), (5,6,1), (4,5,9), (9,3,1), (3,5,8)]
+for each in temp:
+    print(each)
+    result = list()
+    for e in each.split("_"):
+        result.append(int(e))
+    print(result)
+'''
+young_and_old_birthdays= {}
+'''
+for each in bDays:
+    year, month, day = map(int, each.split("_"))
+    date = (month, day)
+    
+    if year not in young_and_old_birthdays:
+        young_and_old_birthdays[year] = []
+    young_and_old_birthdays[year].append(date)
 
-def checker(givenList: list) -> None:
-    for triangle in givenList:
-        a, b, c = triangle
-        if (a + b > c) and (a + c > b) and (b + c > a):
-            print(f"{triangle}: True")  
-        else:
-            print(f"{triangle}: False")
-
-checker(triangleList)
+for year, dates in sorted(young_and_old_birthdays.items()):
+    earliest_date = min(dates)  
+    latest_date = max(dates)    
+    print(f"Year: 20{year:02d}")
+    print(f"Oldest Date: {earliest_date[0]:02d}-{earliest_date[1]:02d}")
+    print(f"Youngest Date: {latest_date[0]:02d}-{latest_date[1]:02d}")
+'''
 
 
 
+for name, bDay in name_birthday_dict.items():
+    year, month, day = map(int, bDay.split("_"))
+    date = (month, day)
 
+    # Add the birth date to the year key, along with the name
+    if year not in young_and_old_birthdays:
+        young_and_old_birthdays[year] = []
+    young_and_old_birthdays[year].append((date, name))
 
+# Print earliest and latest birth dates for each year
+for year, dates in sorted(young_and_old_birthdays.items()):
+    # Sort dates within each year to get the earliest and latest with names
+    sorted_dates = sorted(dates)
+    earliest_date, earliest_name = sorted_dates[0]
+    latest_date, latest_name = sorted_dates[-1]
+
+    print(f"Year: 20{year:02d}")
+    print(f"Oldest Date: {earliest_date[0]:02d}-{earliest_date[1]:02d}, Name: {earliest_name}")
+    print(f"Youngest Date: {latest_date[0]:02d}-{latest_date[1]:02d}, Name: {latest_name}")
